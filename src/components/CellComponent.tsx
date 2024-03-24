defb755ff185b998ca54330ab6447fac5e10f898
3TERRRNITY <qertisww@gmail.com>
2024-03-24 19:51:22 +0300

    add figure-cell logic

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,5 +1,12 @@
-const CellComponent = () => {
-  return <div className="cell">CellComponent</div>;
+import { FC } from "react";
+import { Cell } from "../models/Cell";
+
+interface CellProps {
+  cell: Cell;
+}
+
+const CellComponent: FC<CellProps> = ({ cell }) => {
+  return <div className={["cell", cell.color].join(" ")}>{cell.figure}</div>;
 };
 
 export default CellComponent;
